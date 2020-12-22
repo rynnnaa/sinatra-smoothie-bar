6206dcb69cd28dc0295038c645fa428b98b18215
Ryna <pinktulips@Hotmail.com>
2020-12-22 11:49:56 -0800

    created sessions and authenticates user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,22 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    erb :login
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      puts session
+      redirect "users/#{@user.id}"
+    else
+
+    end
   end
 
-  
+  get '/users/:id' do
+    "This is the user show page"
+  end
+
+
+
   get '/signup' do
     erb :signup
   end
