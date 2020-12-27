7903124ce8d5441c7875f910fbbe0e1108aa24ec
Ryna <pinktulips@Hotmail.com>
2020-12-27 12:04:26 -0800

    created logged in logic and redirects

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,10 +1,29 @@
 class SmoothiesController < ApplicationController
 
   get '/smoothies/new' do
-    erb :"/smoothies/new"
+    erb :'/smoothies/new'
   end
 
-  post '/smoothies/new' do
-    erb :"/smoothies/new"
+  post '/smoothies' do
+    if !logged_in?
+      redirect '/'
+    end
+
+    if params[:content] !=""
+      @smoothie = Smoothie.create(content: params[:content], user_id: current_user.id)
+      redirect "/smoothies/#{@smoothie.id}"
+    else
+      redirect "/smoothies/new"
+    end
   end
 end
+
+#   get '/smoothies/:id' do
+#     @smoothie = Smoothie.find(params[:id])
+#     erb :"/smoothies/show"
+#   end
+
+#   get '/smoothies/:id/edit' do 
+#     "Hello World"
+#   end
+# end
