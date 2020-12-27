c29885ea419fca27b028181e0609f551017811b7
Ryna <pinktulips@Hotmail.com>
2020-12-27 08:50:11 -0800

    created a post and get request for smoothies

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,2 +1,10 @@
 class SmoothiesController < ApplicationController
+
+  get '/smoothies/new' do
+    erb :"/smoothies/new"
+  end
+
+  post '/smoothies/new' do
+    erb :"/smoothies/new"
+  end
 end
